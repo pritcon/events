e8ad27d589cc0107b674f30fec07f9901c96bcd1
pritcon <pritam_banerjee@condenast.com>
2022-06-17 22:15:27 +0530

    api code added

--- pages/sponsorship.js
+++ pages/sponsorship.js
@@ -18,20 +18,34 @@ export default function Index({ content }) {
         <div className="c-page-sections u-spacing--sections">
           <section className="l-container l-wrap l-wrap--full">
           <div class="bg-loader"></div>
-          <h1>Removed component</h1>
+          <SlickSlider>
+              {content?.videoEmbedUrl ? (
+                <VideoPlayer url={content.videoEmbedUrl} settings={{playsinline: true, loop: true, controls: false, volume: 0, muted: true, playing: true}} />
+              ) : (
+                getFilteredAssets(content.imagesCollection.items).map((media, index) => (
+                  <div key={index}>
+                    { media && media.contentType.includes("video/") ? (
+                      <VideoPlayer url={media && media.url} settings={{playsinline: true, loop: true, controls: false, volume: 0, muted: true, playing: true}}/>
+                    ) : (
+                      <img src={`${media && media.url}?fit=fill&w=1600&h=900`} alt={media && media.title} />
+                    )}
+                  </div>
+                ))
+              )}
+            </SlickSlider>
           </section>
 
           <SectionEventContent
             name={content?.displayTitle ?? content?.title}
             body={content?.body}
-            buttons={content?.button ? [content?.button] : []}
+            buttons={content?.button && Array.isArray(content?.button) && content?.button.length ? [content?.button[0]] : []}
           />
           <section className="c-section-sponsorship o-section l-container l-wrap u-spacing--triple">
             <SectionHeading
               text="Our Impact"
             />
             <SectionStats
-              stats={content?.stats}
+              stats={content?.stat}
             />
           </section>
           <section className="c-section-sponsorship-content o-section l-container l-wrap u-spacing--triple">
@@ -39,7 +53,7 @@ export default function Index({ content }) {
               text="Opportunities"
             />
             <div bp="grid 6@md">
-              {content?.sponsorships?.length > 0 && content?.sponsorships.items.map(sponsoship => (
+              {content?.sponsorships?.length > 0 && content?.sponsorships.map(sponsoship => (
                 <React.Fragment key={sponsoship.key}>
                   <div className="c-section-sponsorship-content__header">
                     <div className="c-section-sponsorship-content__heading u-flex">
@@ -70,9 +84,9 @@ export default function Index({ content }) {
 }
 
 export async function getStaticProps({ params, preview = false }) {
-
+  console.log('checcccc')
   let content = await getSponsorshipPageContentStrapi();
-  content = content ? content : {}
+  content = content && Array.isArray(content) && content.length ? content[0] : {}
   console.log('conetne checkkk');
   console.log(content)
   return {
